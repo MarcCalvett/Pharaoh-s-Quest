ef6ddb400d996342a2dc6770d30df869f7826487
Pau Amigó <pau.amigo@enti.cat>
2022-04-05 20:55:33 +0200

    Escenes del parallax i codi parcialment acabat

---
+++ Assets/Scripts/Background/ParallaxEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private Transform cameraTransform;
+    private Vector3 previousCameraPosition;
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        previousCameraPosition = cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        float diferenceX=(cameraTransform.position.x - previousCameraPosition.x)
+        transform.Translate(new Vector3(diferenceX, 0, 0));
+        previousCameraPosition = cameraTransform.postion;
+    }
+}
