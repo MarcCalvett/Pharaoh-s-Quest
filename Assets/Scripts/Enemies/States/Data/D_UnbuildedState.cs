fe58eefc663b42243abee977f406104cf3a52a98
marccalvet <marc.calvet@enti.cat>
2022-04-05 18:06:34 +0200

    nou state començat

---
+++ Assets/Scripts/Enemies/States/Data/D_UnbuildedState.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newUnbuildedStateData", menuName = "Data/StateData/UnbuildedState")]
+public class D_UnbuildedState : ScriptableObject
+{
+    
+}
