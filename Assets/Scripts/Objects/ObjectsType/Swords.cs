03d387909f160f897a3af57bf9bf9401ab4f7a66
Marc Calvet <marc.calvet@enti.cat>
2022-05-02 00:44:16 +0200

    Lvl2.1 espases + InteractableObjects

---
+++ Assets/Scripts/Objects/ObjectsType/Swords.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swords : Interactable
+{
+    public BoolValue swordsTaken;  
+    
+    void Update()
+    {
+        if (swordsTaken.RuntimeValue)
+        {
+            Destroy(this.gameObject);
+        }
+        else if (Input.GetKeyDown(KeyCode.B) && playerInRange)
+        {
+            swordsTaken.RuntimeValue = true;
+            Destroy(this.gameObject);
+        }
+    }
+    
+
+
+}
